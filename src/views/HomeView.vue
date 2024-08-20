606b4ed10bc87ce8cbfa938eabd2d09a1c479f1a
Tejesh Alimilli <tejesh-alimilli@users.noreply.github.com>
2024-08-20 19:12:49 -0400

    created home page with api key and location name fields

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWeatherAppStore } from '@/stores/weather'
+import { ref } from 'vue'
+
+const store = useWeatherAppStore()
+
+let apiKey = ref(store.apiKey)
+let locationName = ref(store.locationName)
+
+function searchClicked() {
+  store.setApiKey(apiKey.value)
+  store.setLocationName(locationName.value)
+  console.log('get weather for ' + locationName.value)
+}
+</script>
 
 <template>
-  <main>Hello World!</main>
+  <h1>Welcome to Weather App</h1>
+  <main>
+    API Key: &nbsp;
+    <!-- To avoid hardcoding API Key made it into a input field, save to local storage for convinience -->
+    <input type="text" placeholder="Enter openweathermap API Key here" v-model="apiKey" />
+    <br /><br />
+    Enter any US City Name: &nbsp;
+    <input type="search" placeholder="Enter US City Name here" v-model="locationName" />
+    <br /><br />
+    <button @click="searchClicked">Go</button>
+  </main>
 </template>
